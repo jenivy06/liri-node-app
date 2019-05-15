ba6d8620ac431f2b9a4c08789245e467ed654ae6
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-05-15 17:53:53 -0600

    completed function code for concert-this argument

--- liri.js
+++ liri.js
@@ -28,4 +28,26 @@ switch (action) {
         doThis(userInput);
         break;
 };
+function concertThis(userInput) {
+    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
+    .then(function(response) {    
+        for (var i = 0; i < response.data.length; i++) {
 
+            // Get concert date and format it to "MM/DD/YYYY"
+            var date = response.data[i].datetime;
+            var convertedDate = (moment().format("MM/DD/YY"));
+            //print concert results to terminal
+            var concertResults = 
+                "--------------------------" +
+                    "\nVenue Name: " + response.data[i].venue.name + 
+                    "\nVenue Location: " + response.data[i].venue.city +
+                    "\nDate of the Event: " + convertedDate
+            console.log(concertResults);
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+        
+
+}
