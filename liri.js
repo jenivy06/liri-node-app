d3d39261c344e0ec2a160bad3ecbef8fecc126ac
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-05-15 19:42:49 -0600

    added doThis function to liri.js

--- liri.js
+++ liri.js
@@ -103,3 +103,23 @@ function movieThis () {
     });
     
 }
+
+// run doThis function
+
+function doThis() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+          }
+
+    // We will then print the contents of data
+  console.log(data);
+
+  // Then split it by commas (to make it more readable)
+  var dataArr = data.split(",");
+
+  // We will then re-display the content as an array for later use.
+  console.log(dataArr);
+
+});
+}
