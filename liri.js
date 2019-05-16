f2c5009a2726581f3e42046aee4761a9baefb815
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-05-15 19:20:00 -0600

    added function for getting spotify api

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+// create initial variables
+
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api'); 
 var spotify = new Spotify(keys.spotify);
@@ -13,6 +15,8 @@ var fs = require('fs');
 
 var action = process.argv[2]; 
 var userInput = process.argv[3]; 
+
+//run function for command line arguments
 
 switch (action) {
     case "concert-this":
@@ -28,6 +32,9 @@ switch (action) {
         doThis(userInput);
         break;
 };
+
+// run concertThis function
+
 function concertThis(userInput) {
     axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
     .then(function(response) {    
@@ -50,4 +57,26 @@ function concertThis(userInput) {
     });
         
 
+}
+
+// run spotifySong function
+
+function spotifySong (userInput) {
+    spotify
+    .search({ type: 'track', query: userInput })
+    .then(function(response) {
+        for (var i = 0; i < 5; i++) {
+            var spotifyResults = 
+            "--------------------------" +
+                    "\nArtist(s): " + response.tracks.items[i].artists[0].name + 
+                    "\nSong Name: " + response.tracks.items[i].name +
+                    "\nAlbum Name: " + response.tracks.items[i].album.name +
+                    "\nPreview Link: " + response.tracks.items[i].preview_url;
+                    
+            console.log(spotifyResults);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
 }
